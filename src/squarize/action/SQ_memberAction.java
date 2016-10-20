97981171784164c46fcabe43244d3b76a16ca138
hooni24 <kshmarine@gmail.com>
2016-10-20 20:58:07 +0900

    화선코드정리
    다 썼으면 sysout이랑 alert좀 지워라~~~~~~~~~~~~~

--- src/squarize/action/SQ_memberAction.java
+++ src/squarize/action/SQ_memberAction.java
@@ -10,9 +10,7 @@ import squarize.dao.SQ_memberDAO;
 import squarize.vo.SQ_member;
 
 public class SQ_memberAction extends ActionSupport implements SessionAware{
-	
 	private SQ_memberDAO mdao;
-	
 	private Map<String, Object> session;
 	private SQ_member sq_member;
 	private String sq_member_id;
@@ -20,6 +18,53 @@ public class SQ_memberAction extends ActionSupport implements SessionAware{
 	private String loginId;
 	private String isArtist;
 
+	
+	
+	public String idCheck() throws Exception{
+		mdao=new SQ_memberDAO();
+		sq_member=mdao.loginSQmember(sq_member_id);
+		return SUCCESS;
+	}
+	
+	public String registerSQmember() throws Exception{
+		mdao=new SQ_memberDAO();
+		mdao.registerSQmember(sq_member);
+		return SUCCESS;
+	}
+	
+	public String loginSQmember() throws Exception{
+		mdao=new SQ_memberDAO();
+		sq_member=mdao.loginSQmember(sq_member_id);
+		
+		if(sq_member.getSq_member_pw().equals(sq_member_pw)){
+			session.put("loginId", sq_member.getSq_member_id());
+			session.put("isArtist", sq_member.getSq_member_isartist());
+			loginId=(String) session.get("loginId");
+			isArtist=(String) session.get("isArtist");
+		}else{
+			sq_member=null;
+			loginId="";
+			isArtist="";
+		}
+		return SUCCESS;
+	}
+	
+	public String loginCheck() throws Exception{
+		loginId=(String)session.get("loginId");
+		isArtist=(String)session.get("isArtist");
+		System.out.println(isArtist);
+		return SUCCESS;
+	}
+	
+	public String logoutSQmember() throws Exception{
+		session.clear();
+		return SUCCESS;
+	}
+	
+	
+	
+	
+	
 	
 	public SQ_member getSq_member() {
 		return sq_member;
@@ -48,63 +93,12 @@ public class SQ_memberAction extends ActionSupport implements SessionAware{
 	public void setLoginId(String loginId) {
 		this.loginId = loginId;
 	}
-	
-
-	
 	public String getIsArtist() {
 		return isArtist;
 	}
 	public void setIsArtist(String isArtist) {
 		this.isArtist = isArtist;
 	}
-	public String idCheck() throws Exception{
-		mdao=new SQ_memberDAO();
-		sq_member=mdao.loginSQmember(sq_member_id);
-		
-			return SUCCESS;
-		
-	}
-	public String registerSQmember() throws Exception{
-		mdao=new SQ_memberDAO();
-
-		mdao.registerSQmember(sq_member);
-
-		return SUCCESS;
-	}
-	
-	public String loginSQmember() throws Exception{
-		mdao=new SQ_memberDAO();
-		sq_member=mdao.loginSQmember(sq_member_id);
-		
-		if(sq_member.getSq_member_pw().equals(sq_member_pw)){
-			session.put("loginId", sq_member.getSq_member_id());
-			session.put("isArtist", sq_member.getSq_member_isartist());
-			loginId=(String) session.get("loginId");
-			isArtist=(String) session.get("isArtist");
-			System.out.println();
-		}else{
-			sq_member=null;
-			loginId="";
-			isArtist="";
-		}
-		return SUCCESS;
-	}
-	public String loginCheck() throws Exception{
-		System.out.println();
-		System.out.println();
-		System.out.println();
-		System.out.println();
-		
-		loginId=(String)session.get("loginId");
-		isArtist=(String)session.get("isArtist");
-		System.out.println(isArtist);
-			return SUCCESS;
-		}
-	public String logoutSQmember() throws Exception{
-		session.clear();
-		return SUCCESS;
-	}
-	
 	@Override
 	public void setSession(Map<String, Object> arg0) {
 		session = arg0;
