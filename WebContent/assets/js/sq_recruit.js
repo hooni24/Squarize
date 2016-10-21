353de0a43743462677444f8cfcf600c3e644fbe1
wkhs4461 <wkhs4461@naver.com>
2016-10-21 22:24:32 +0900

    ad

--- WebContent/assets/js/sq_recruit.js
+++ WebContent/assets/js/sq_recruit.js
@@ -2,5 +2,13 @@
  * 
  */
 $(function(){
-
+	$('.form-group button').click(function(){
+		$.ajax({
+			method:'get'
+			,url:'insertSQrecruit'
+			,success:function(send){
+				alert(send.loginId);
+			}
+		});
+	});
 })
